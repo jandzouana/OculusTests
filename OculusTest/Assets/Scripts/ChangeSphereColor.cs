cb89b01fc17060c92872f256c52c4096e9513306
Jessica Andzouana <jandzouana@gmail.com>
2017-04-28 11:43:06 -0700

    added audio interaction

--- OculusTest/Assets/Scripts/ChangeSphereColor.cs
+++ OculusTest/Assets/Scripts/ChangeSphereColor.cs
@@ -6,33 +6,30 @@ public class ChangeSphereColor : MonoBehaviour {
     private Material originalColor;
     public Material newColor;
     public ButtonInteract buttonScript;
+    public SceneUtilities utilitiesScript;
     private bool changedColor;
     private bool canTrigger;
+
 
 	// Use this for initialization
 	void Start () {
         originalColor = gameObject.GetComponent<Renderer>().material;
         changedColor = false;
-        canTrigger = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log("on: " + buttonScript.on);
         if (buttonScript.on && buttonScript.switchTrigger) { 
             gameObject.GetComponent<Renderer>().material = originalColor;
             gameObject.GetComponent<AudioSource>().Stop();
-            canTrigger = false;
             changedColor = false;
         }
         else if (buttonScript.switchTrigger)
         {
             gameObject.GetComponent<Renderer>().material = newColor;
+            utilitiesScript.StopAllAudio();
             gameObject.GetComponent<AudioSource>().Play();
             changedColor = true;
         }
-
-
-
     }
 }
